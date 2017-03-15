7ea02a8b95263f824a0479575589dbf59802850c
Wendy Reyes <wendy.reyessan@gmail.com>
2017-03-15 11:39:31 -0500

    editando js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,42 @@
 
-//window.addEventListener('load',function() {
-var registrar = document.getElementById('submit');
-var pacientes = new Array();
+var registrar = document.getElementById('submit');  //boton
+var pacientes = new Array();  //Crea Array vacio
 
-    registrar.addEventListener('click',function() {
+    registrar.addEventListener('click',function(event) {
+    event.preventDefault();  // evita que se limpie automaticamente
           var nombre = document.getElementById('nombre').value;
           var apellido = document.getElementById('apellido').value;
           var edad = document.getElementById('edad').value;
           var genero = document.getElementById('genero').value;
           var ciudad = document.getElementById('ciudad').value;
           var pais = document.getElementById('pais').value;
+          var imprimir = document.getElementById('print');
+
+          pacientes.push(new Paciente(nombre, apellido, edad, genero, ciudad, pais));  //inserta objeto nuevo en Array
+          imprimir.appendChild(pacientes[pacientes.length -1].mostrarHtml());   //mostrando ultimo objeto de Array con su metodo mostrar
+          document.getElementById("ficha-medica").reset();  // limpiar formulario
 
   });
 
 
-function Paciente(nombre, apellido, edad, genero, ciudad, pais){
+  function Paciente(nombre, apellido, edad, genero, ciudad, pais){
 
-this.nombre = nombre;
-this.apellido = apellido;
-this.edad = edad;
-this.genero = genero;
-this.ciudad = ciudad;
-this.pais = pais;
-this.mostrarHtml = function(){
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.edad = edad;
+  this.genero = genero;
+  this.ciudad = ciudad;
+  this.pais = pais;
+  this.mostrarHtml = function(){
 
-                    var ficha = document.createElement('div');
-                    var datos = document.createElement('p');
-                    datos.innerHTML = "Nombre: "+ this.nombre+ "<br>Edad: " + this.edad+"<br>País: " +this.pais;
+                      var ficha = document.createElement('div');
+                      var datos = document.createElement('p');
+                      datos.innerHTML += "Nombre: "+this.nombre+
+                                         "<br>Edad: " +this.edad+
+                                         "<br>País: " +this.pais;
 
-                    ficha.appendChild(datos);
-                    return ficha;
-                    };
+                      ficha.appendChild(datos);
+                      return ficha;
+                      };
 
-  var imprimir = document.getElementById('print');
-  print.appendChild(ficha());
-
-}
+  }
